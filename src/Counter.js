922b2f17b575e6ae43fc578329b3d0c540650b7d
= <hsyoon1799@naver.com>
2021-06-21 15:15:24 +0900

    01_24

--- src/Counter.js
+++ src/Counter.js
@@ -1,34 +1,86 @@
-import React, { useReducer } from 'react';
+// 클래스형컴포넌트
+import React, { Component } from 'react';
 
-function reducer(state, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1;
-    case 'DECREMENT':
-      return state - 1;
-    default:
-      return state;
+class Counter extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.handleIncrease = this.handleIncrease.bind(this);
+  //   this.handleDecrease = this.handleDecrease.bind(this);
+  // }
+
+  // handleIncrease() {
+  //   console.log('increase');
+  //   console.log(this);
+  // }
+  
+  // handleDecrease() {
+  //   console.log('decrease');
+  //   console.log(this);
+  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+
+  handleIncrease = () => {
+    this.setState({
+      counter: this.state.counter + 1
+    });
+  };
+
+  handleDecrease = () => {
+    this.setState({
+      counter: this.state.counter -1
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>{this.state.counter}</h1>
+        <button onClick={this.handleIncrease}>+1</button>
+        <button onClick={this.handleDecrease}>-1</button>
+      </div>
+    );
   }
 }
 
-function Counter(){
-  const [number, dispatch] = useReducer(reducer, 0);
-
-  const onIncrease = () => {
-    dispatch({ type: 'INCREMENT' });
-  };
-
-  const onDecrease = () => {
-    dispatch({ type: 'DECREMENT' });
-  };
-
-  return (
-    <div>
-      <h1>{number}</h1>
-      <button onClick={onIncrease}>+1</button>
-      <button onClick={onDecrease}>-1</button>
-    </div>
-  );
-}
-
 export default Counter;
+
+// 함수형 컴포넌트
+// import React, { useReducer } from 'react';
+
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// }
+
+// function Counter(){
+//   const [number, dispatch] = useReducer(reducer, 0);
+
+//   const onIncrease = () => {
+//     dispatch({ type: 'INCREMENT' });
+//   };
+
+//   const onDecrease = () => {
+//     dispatch({ type: 'DECREMENT' });
+//   };
+
+//   return (
+//     <div>
+//       <h1>{number}</h1>
+//       <button onClick={onIncrease}>+1</button>
+//       <button onClick={onDecrease}>-1</button>
+//     </div>
+//   );
+// }
+
+// export default Counter;
--- src/Hello.js
+++ src/Hello.js
@@ -1,28 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function Hello({ color, name, isSpecial }){
-  return(
-    <div style={{ color }}>
-    { isSpecial && <b>*</b> } 
-    {/* console.log(true && 'hello'); // hello
-console.log(false && 'hello'); // false
-console.log('hello' && 'bye'); // bye
-console.log(null && 'hello'); // null
-console.log(undefined && 'hello'); // undefined
-console.log('' && 'hello'); // ''
-console.log(0 && 'hello'); // 0
-console.log(1 && 'hello'); // hello
-console.log(1 && 1); // 1 
+class Hello extends Component {
+  static defaultProps = {
+    name: '이름없음'
+  };
+  
+  render() {
+    const { color, name, isSpecial } = this.props;
 
-https://learnjs.vlpt.us/useful/03-short-circuiting.html
-*/}
-    안녕하세요 {name}
-    </div>
-  );
+    return (
+      <div style={{ color }}>
+        {isSpecial && <b>*</b>}
+        안녕하세요 {name}
+      </div>
+    );
+  }
 }
 
-Hello.defaultProps = {
-  name: '이름없음'
-}
+// Hello.defaultProps = {
+//   name: '이름없음'
+// };
 
 export default Hello;
